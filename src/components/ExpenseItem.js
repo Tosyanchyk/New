7d9d11993f7cd993132954e946c0d21fbc0b8f75
Tosyanchyk <ddd.tosha@gmail.com>
2022-04-01 22:15:04 +0300

    keep checking

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -10,8 +10,6 @@ export default function ExpenceItem(props) {
         <h2> {props.title} </h2>
         <div className="expense-item__price"> $ {props.amount} </div>
       </div>
-      <div></div>
     </Card>
-    
   );
 }
