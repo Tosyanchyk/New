cced90e40c0dda0e2feeacdb7566bb3365c6128c
Tosyanchyk <ddd.tosha@gmail.com>
2022-04-05 03:26:20 +0300

    adding two-way binding and working with form

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,60 +1,95 @@
 import "./ExpenseForm.css";
-import { useState } from "react";//55
-
+import { useState } from "react"; //55
 
 const ExpenseForm = () => {
-    // const [enteredTitle, setEnteredTitle] = useState(''); // but i can go another way
-    // const [enteredAmount, setEnteredAmount] = useState('');
-    // const [enteredDate, setEnteredDate] = useState('');
+  const [enteredTitle, setEnteredTitle] = useState(""); // but i can go another way
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
 
-    const [userInput, setUserInput] = useState({
-        enteredTitle: '',
-        enteredAmount: '',
-        enteredDate: ''
-    });
+  // const [userInput, setUserInput] = useState({
+  //   enteredTitle: "",
+  //   enteredAmount: "",
+  //   enteredDate: "",
+  // });
 
-    const titleChangeHandler = (event) => {
-        // setEnteredTitle(event.target.value);
-        setUserInput({
-            ...userInput,
-            enteredTitle: event.target.value,
-        })
+  const titleChangeHandler = (event) => {
+    setEnteredTitle(event.target.value);
+    //   setUserInput({
+    //     ...userInput,
+    //     enteredTitle: event.target.value,
+    // });
+  };
+
+  //   setUserState({ // do this when my state depends on previous state
+  //     return {..prevState, enteredTitle: event.target.value};
+  //   });
+  // };
+
+  const amountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value);
+    //   setUserInput({
+    //     ...userInput,
+    //     enteredAmount: event.target.value,
+    //   });
+  };
+
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
+    //   setUserInput({
+    //     ...userInput,
+    //     enteredDate: event.target.value,
+    //   });
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const expenseData = {
+      // don't need to combine while i use alternative way of useState declaration
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
     };
-
-    const amountChangeHandler = (event) =>{
-        // setEnteredAmount(event.target.value);
-        setUserInput({
-            ...userInput,
-            enteredAmount: event.target.value,
-        })
-    };
-
-    const dateChangeHandler = event => {
-        // setEnteredDate(event.target.value);
-        setUserInput({
-            ...userInput,
-            enteredDate: event.target.value,
-        })
-    };
+    console.log(expenseData);
+    setEnteredTitle('');
+    setEnteredAmount('');
+    setEnteredDate('');
+  };
 
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__controls">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler}/>
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          />
         </div>
         <div className="new-expense__controls">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" cnChange={amountChangeHandler}/>
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            value={enteredAmount}
+            onChange={amountChangeHandler}
+          />
         </div>
         <div className="new-expense__controls">
           <label>Date</label>
-          <input type="date" min='2019-01-01' max='2022-12-31' onChange={dateChangeHandler}/>
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2022-12-31"
+            value={enteredDate}
+            onChange={dateChangeHandler}
+          />
         </div>
       </div>
       <div className="new-expense__actions">
-        <button type='submit'>Add Expense</button>
+        <button type="submit">Add Expense</button>
       </div>
     </form>
   );
