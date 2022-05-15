06d0d48850d8278535453098666203fc9193ea9a
Tosyanchyk <ddd.tosha@gmail.com>
2022-05-15 20:23:54 +0300

    adding keys to map method

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -7,7 +7,7 @@ import "./Expenses.css";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
-
+  console.log(props.items);
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
@@ -21,6 +21,7 @@ const Expenses = (props) => {
         />
         {props.items.map((expense) => (
           <ExpenseItem
+            key={expense.id}
             title={expense.title}
             amount={expense.amount}
             date={expense.date}
